5234ef7bba8803adbbe11ae0cfb9c6d82ff5508e
Lesia <leskinkotik@gmail.com>
2024-06-05 15:42:17 +0200

    🐛 error was corrected

--- tests/test_math.py
+++ tests/test_math.py
@@ -1,7 +1,8 @@
 from app.controllers.math import inc, minus, power
 
 def test_inc():
-    assert inc(3) == 567890
+    assert inc(3) == 4
 
 def test_minus():
-    assert minus(3) == 2   
+    assert minus(3) == 2
+    
